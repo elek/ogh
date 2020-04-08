5da215866cce6091b8536f430502da4e0abfd2c3
Elek Márton <elek@apache.org>
2020-04-08 09:25:24 +0200

    add method to get workflows

--- githubapi.go
+++ githubapi.go
@@ -14,13 +14,22 @@ func GetArtifacts(org string, repo string, runId string) (map[string]interface{}
 	return asJson(cachedGet3min(apiGetter, org+"-"+repo+"-"+"-actions-runs-"+runId+"-artifacts"))
 }
 
+func GetWorkflowRunsOfBranch(org string, repo string, workflowId string, branch string) (map[string]interface{}, error) {
+	cacheKey := org + "-" + repo + "-actions-workflows-" + workflowId + "-runs"
+	url := "https://api.github.com/repos/" + org + "/" + repo + "/actions/workflows/" + workflowId + "/runs?per_page=100"
+	if branch != "" {
+		cacheKey += "-" + branch
+		url += url + "&branch=" + branch
+	}
+	apiGetter := func() ([]byte, error) {
+		return readGithubApiV3(url)
+	}
 
+	return asJson(cachedGet3min(apiGetter, cacheKey))
+}
 
 func GetWorkflowRuns(org string, repo string, workflowId string) (map[string]interface{}, error) {
-	apiGetter := func() ([]byte, error) {
-		return readGithubApiV3("https://api.github.com/repos/" + org + "/" + repo + "/actions/workflows/" + workflowId + "/runs")
-	}
-	return asJson(cachedGet3min(apiGetter, org+"-"+repo+"-actions-workflows-"+workflowId+"-runs"))
+	return GetWorkflowRunsOfBranch(org, repo, workflowId, "")
 }
 
 func GetPrCommits(org string, repo string, pullId string) ([]interface{}, error) {
