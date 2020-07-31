7e9d189d17258bf42c787a2ba5c1cb6507874c77
Elek Márton <elek@apache.org>
2020-07-31 08:42:19 +0200

    add new build -> artifact mapping

--- ozone.go
+++ ozone.go
@@ -3,10 +3,14 @@ package main
 var artifactMap = map[string]string{
 	"integration (freon)":               "it-freon",
 	"integration (filesystem)":          "it-filesystem",
+	"integration (filesystem-hdds)":     "it-filesystem-hdds",
 	"integration (filesystem-contract)": "it-filesystem-contract",
 	"integration (client)":              "it-client",
 	"integration (hdds-om)":             "it-hdds-om",
 	"integration (ozone)":               "it-ozone",
+	"acceptance (misc)":                 "acceptance-misc",
+	"acceptance (secure)":               "acceptance-secure",
+	"acceptance (unsecure)":             "acceptance-unsecure",
 }
 
 func JobToArtifactName(job string) string {
